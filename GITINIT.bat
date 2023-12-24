cc774de08f3fef1af7d4d62bd7eb4823eb57ac55
pete willard <petewillard@gmail.com>
2023-12-24 09:46:49 -0500

    reconfig folders, added up to page 28

--- GITINIT.bat
+++
@@ -1,7 +0,0 @@
-@echo on
-git init
-git lfs install --local
-git lfs track *.ttf, *.afpub, *.jpg, *.jpeg, *.tif, *.gif, *.bmp, *.svg, *.afdesign, *.pdf, *.png
-git add .gitattributes
-git commit -m "added .gitattributes"
-pause
